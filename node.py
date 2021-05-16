959f010d8e89ba63f8d8e4bf94b1425b23c82d41
Dean <puffin.develop@gmail.com>
2021-05-16 13:08:35 +0100

    Restructure filesystem

--- node.py
+++ node.py
@@ -4,7 +4,7 @@ import threading
 
 from flask import Flask
 from tkinter import *
-from blockchain import *
+from puffincoin.blockchain import *
 
 
 app = Flask(__name__)
@@ -23,6 +23,13 @@ def send_chain():
 
     return json.dumps(response)
 
+@app.route('/transaction', methods=['GET'])
+def new_tx():
+   key = blockchain.generate_keys()
+   blockchain.add_transaction("Bob", "Joe", 1000, key, key)
+   blockchain.add_transaction("Joe", "Bob", 10, key, key)
+   print(blockchain)
+   return "Added."
 
 if __name__ == '__main__':
     app.run(debug=True, port=8222)
